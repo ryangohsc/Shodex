bd1dd1a36ea755b5ec7bc745e2c7ae53e3cdc5d0
Ryan Goh <1802980@sit.singaporetech.edu.sg>
2022-02-23 21:31:06 +0800

    Finalised the nmap module

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from modules.nmap import *
 from modules.local_cve_parser import *
 
 # SHODAN_API_KEY = "OooeRjrCHdbDI98zZV8VQqhoTT6WCqoc"
-# gSQ3nesmWGafxG3xX8U3mP6YE8dcaJeK
+# backup key: gSQ3nesmWGafxG3xX8U3mP6YE8dcaJeK
 
 # Global Variables
 API_KEY_POS = 0
@@ -19,10 +19,12 @@ def splashscreen():
 
 def init_arg_parser():
     parser = argparse.ArgumentParser(description="ICT2206 - Project Shodex", epilog="ICT2206 Assignment 1 Team x")
-    parser.add_argument("--target", help="Target IP (e.g. 192.168.1.2 / 192.168.1.1-8 / 192.168.1.0/24)", required=True)
+    parser.add_argument("--target", help="Target IP (e.g. 192.168.1.2 / 192.168.1.1-8 / 192.168.1.0/24)",
+                        required=False)
     parser.add_argument("--speed", help="Speed of the offline scan (e.g. quick or through)", required=False)
     parser.add_argument("--api_key", help="Shodan API key", required=False)
     parser.add_argument("--filter", help="Search filter", required=False)
+    parser.add_argument("--ondemand", help="Shodan on-demand scan a target", required=False)
     args = parser.parse_args()
     return args
 
@@ -37,16 +39,24 @@ def main():
     speed = args.speed
     api_key = args.api_key
     filter = args.filter
+    ondemand = args.ondemand
 
     # Initiate online mode.
-    if api_key != None:
-        pass
+    if api_key is not None:
         # Initiate Shodan API and run its functions
-    #    print("[!] Initiating an online scan!")
-    #    shodan_app_obj = ShodanAPI(api_key, filter)
-    #    shodan_app_obj.check_api_info()
-    #    if shodan_app_obj.scan_filter():
-    #        shodan_app_obj.scan_specified_ip()
+        shodan_app_obj = ShodanAPI(api_key, filter)
+        shodan_app_obj.check_api_info()
+
+        # On-demand scan mode.
+        if ondemand is not None:
+            print("[!] Initiating an ondemand scan!")
+            shodan_app_obj.on_demand_scan(ondemand)
+
+        # Search mode.
+        else:
+            print("[!] Retrieving results from Shodan!")
+            if shodan_app_obj.scan_filter():
+                shodan_app_obj.scan_specified_ip()
 
     # Initiate offline mode
     else:
@@ -54,16 +64,15 @@ def main():
         nmap = Nmap()
         if speed is None:
             speed = "quick"
-        cve_list = nmap.run(target, speed)
+        service_list = nmap.run(target, speed)
 
         # Check if there are any services found
-        if cve_list == {}:
+        if service_list == {}:
             print("[!] No services found!")
-
-        # If there are no services found.
         else:
             cve_parser = LocalCveParser()
-            cve_parser.run(cve_list)
+            cve_list = cve_parser.run(service_list)
+            # Feed the cve_list to the exploit crawler here.
 
 
 if __name__ == '__main__':
